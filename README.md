655e6a82b8e3fa3fea204674f004895c98b41a3f
Camilla Eriksson <ericsson.camilla@gmail.com>
2019-11-08 13:30:00 +0100

    Code cleanup, added scroll on link

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Portfolio
 My personal portfolio website
+
+Particles javascript library (particles.js and app.js) from:
+https://github.com/VincentGarreau/particles.js/
+
--- script.js
+++ script.js
@@ -2,9 +2,19 @@ const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
 
-hamburger.addEventListener("click", () => {
+hamburger.addEventListener("click", () => toggleLinks());
+
+links.forEach(l => l.addEventListener("click", () => toggleLinks()));
+
+function toggleLinks() {
     navLinks.classList.toggle("open");
     links.forEach(link => {
         link.classList.toggle("fade");
-    })
-})
+    });
+}
+
+$('.about_link').click(() => {
+    $('html, body').animate({
+        scrollTop: $('.about').offset().top - $('nav').height()
+    }, 1000);
+});
