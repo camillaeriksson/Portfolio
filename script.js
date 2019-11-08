1fda5c30268bcb3bbceb87883b4bd411bd8a4084
Camilla Eriksson <ericsson.camilla@gmail.com>
2019-11-08 15:41:52 +0100

    Added style sheets

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const links = document.querySelectorAll(".nav-links li");
 
 hamburger.addEventListener("click", () => toggleLinks());
 
-links.forEach(l => l.addEventListener("click", () => toggleLinks()));
+links.forEach(link => link.addEventListener("click", () => toggleLinks()));
 
 function toggleLinks() {
     navLinks.classList.toggle("open");
@@ -16,5 +16,17 @@ function toggleLinks() {
 $('.about_link').click(() => {
     $('html, body').animate({
         scrollTop: $('.about').offset().top - $('nav').height()
+    }, 1000);
+});
+
+$('.contact_link').click(() => {
+    $('html, body').animate({
+        scrollTop: $('.contact').offset().top - $('nav').height()
+    }, 1000);
+});
+
+$('.portfolio_link').click(() => {
+    $('html, body').animate({
+        scrollTop: $('.portfolio').offset().top - $('nav').height()
     }, 1000);
 });
