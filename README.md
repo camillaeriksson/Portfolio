327268ee3355c95af6f5dac00b63229b692779eb
Camilla Eriksson <ericsson.camilla@gmail.com>
2019-11-15 11:24:57 +0100

    More ScrollMagic

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My personal portfolio website
 Particles javascript library (particles.js and app.js) from:
 https://github.com/VincentGarreau/particles.js/
 
-ScrollMagic API is used.
+ScrollMagic library is used.
--- script.js
+++ script.js
@@ -10,39 +10,39 @@ function initScrollMagicScene() {
     const controller = new ScrollMagic.Controller();
 
 
-    const scene2 = new ScrollMagic.Scene({
-        triggerElement: ".contact_heading",
-        triggerHook: 'onEnter', offset: 150
-    })
-        .setClassToggle(".contact_heading", "show_heading")
-        .addTo(controller);
-
-    const scene3 = new ScrollMagic.Scene({
-        triggerElement: ".contact_text",
-        triggerHook: 'onEnter', offset: 95
-    })
-        .setClassToggle(".contact_text", "show_text")
-        .addTo(controller);
-
-    const scene4 = new ScrollMagic.Scene({
+    const scene1 = new ScrollMagic.Scene({
         triggerElement: ".icon",
         triggerHook: 'onEnter', offset: 150
     })
         .setClassToggle(".icon", "show_icon")
         .addTo(controller);
 
-    const scene5 = new ScrollMagic.Scene({
+    const scene4 = new ScrollMagic.Scene({
         triggerElement: ".about_heading",
-        triggerHook: 'onEnter', offset: 190
+        triggerHook: 'onEnter', offset: 150
     })
-        .setClassToggle(".about_heading", "show_heading")
+        .setClassToggle(".about_heading", "show_heading_and_text")
+        .addTo(controller);
+
+    const scene3 = new ScrollMagic.Scene({
+        triggerElement: ".about_text",
+        triggerHook: 'onEnter', offset: 48
+    })
+        .setClassToggle(".about_text", "show_heading_and_text")
+        .addTo(controller);
+
+    const scene5 = new ScrollMagic.Scene({
+        triggerElement: ".contact_heading",
+        triggerHook: 'onEnter', offset: 150
+    })
+        .setClassToggle(".contact_heading", "show_heading_and_text")
         .addTo(controller);
 
     const scene6 = new ScrollMagic.Scene({
-        triggerElement: ".about_text",
-        triggerHook: 'onEnter', offset: 190
+        triggerElement: ".contact_text",
+        triggerHook: 'onEnter', offset: 95
     })
-        .setClassToggle(".about_heading", "show_heading")
+        .setClassToggle(".contact_text", "show_heading_and_text")
         .addTo(controller);
 }
 
